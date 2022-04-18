0a63981c647047638188f4a7d84d5bad2b3e2868
Sergei Pavlov <gsergius@mail.ru>
2022-04-19 00:15:38 +0300

    vote payment, finish and withdraw functions are done

--- contracts/VotingFactory.sol
+++ contracts/VotingFactory.sol
@@ -5,111 +5,141 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 //@title Voting system
 //@author Mad Aekauq
-//@notice Owner can start votings with the list of candidates.
+//@notice Owner can start _votings with the list of candidates.
 //Then everone can vote, and after a voting finishes the winner gets the reward.
 contract VotingFactory is Ownable {
 
-    //@dev keeps details about the voting
+    enum VotingState {
+        InProcess,
+        CalculatingResults,
+        Finished
+    }
+
     struct Voting {
         uint32 endDate;
-        bool finished;
+        VotingState state;
         address winner;
         address[] candidates;
     }
-    //@dev keeps details about someone's vote (their decision)
+    
     struct Vote {
         uint key;
-        uint votingId;
         address owner;
         address candidate;
     }
 
-    //@dev default duration of all votings
-    uint constant VOTINGS_DURATION = 3 days;
-    //@dev default limit for voters to vote in each voting
-    uint constant VOTES_LIMIT_PER_VOTER = 1;
- 
-    Voting[] public votings;
+    uint internal _votingDuration = 3 days;
+    uint internal _votesLimitPerVoter = 1;
 
-    //@dev all votes to do calculations
-    mapping(uint => Vote[]) _givenVotes;
-    //@dev counters to check if a voter already voted in a voting
-    mapping(uint => uint) _voteCounters;
+    uint internal _voteFee = 0.01 ether;
+    uint internal _comission = 10;//%
 
-    //@notice details about the requested voting
-    function getVotingDetails(uint id) external view returns (Voting memory, Vote[] memory) {
-        return (votings[id], _givenVotes[id]);
+    Voting[] internal _votings;
+    
+    mapping(uint => uint) _balance;//votingId => balance
+    mapping(uint => Vote[]) internal _givenVotes;//votingId => givenVotes
+    mapping(uint => uint) internal _voteCounters;//Vote.key => counter
+
+    //@dev useful for testing
+    function setVotingDuration(uint newDuration) external onlyOwner {
+        _votingDuration = newDuration;
     }
 
-    //@notice gives info about the requested voting
+    //@notice returns details about the requested voting
+    function getVotingDetails(uint id) 
+        external 
+        view 
+        returns (Voting memory, Vote[] memory) 
+    {
+        return (_votings[id], _givenVotes[id]);
+    }
+
+    //@notice returns all votings
     function getVotings() external view returns (Voting[] memory) {
-        return votings;
+        return _votings;
     }
 
     //@dev starts a new voting with the listed candidates and 
     //calculating the end date
     function addVoting(address[] memory candidates) external onlyOwner {
-        require(candidates.length > 0);
+        require(candidates.length > 1, "at least 2 candidates expected");
         
         Voting memory newOne;
-        newOne.endDate = uint32(block.timestamp + VOTINGS_DURATION);
+        newOne.endDate = uint32(block.timestamp + _votingDuration);
         newOne.candidates = candidates;
-        votings.push(newOne);
+        newOne.state = VotingState.InProcess;
+        _votings.push(newOne);
     }
 
     //@dev sends accumulated fees to the owner
-    // function withdraw() external onlyOwner {
-    //     //TODO 1: returns accumulated fees
-    // }
+    function withdraw(uint votingId) external onlyOwner {
+        require(
+            _votings[votingId].state == VotingState.Finished, 
+            "Voting isn't finished yet"
+        );
+        _transfer(_balance[votingId], owner(), votingId);
+    }
 
     //@dev checks if the voting isn't ended yet, 
     //if the voter hasn't voted yet and then saves voter's decision 
-    function vote(uint votingId, address candidate) external {
-        require(votings[votingId].endDate > block.timestamp);
+    function vote(uint votingId, address candidate) external payable {
+        require(msg.value == _voteFee, "Wrong value");
+        require(
+            _votings[votingId].endDate > block.timestamp,
+            "Voting period ended"
+        );
 
         uint key = uint(keccak256(abi.encodePacked(votingId, msg.sender)));
-        require(_voteCounters[key] < VOTES_LIMIT_PER_VOTER);
+        require(
+            _voteCounters[key] < _votesLimitPerVoter,
+            "Votes limit is exceeded"
+        );
         
         _voteCounters[key]++;
-        _givenVotes[votingId].push(Vote(key, votingId, msg.sender, candidate));
+        _givenVotes[votingId].push(Vote(key, msg.sender, candidate));
+        _balance[votingId] += msg.value;
     }
 
     //@dev checks if the voting exists and the end date has come already,
     //finds and saves the winner and sends the reward
     function finish(uint votingId) external {
+        Voting storage voting = _votings[votingId];
         require(
-            !votings[votingId].finished, 
-            "the voting has been finished already"
+            voting.state == VotingState.InProcess, 
+            "The voting finished already"
         );
         require(
-            votings[votingId].endDate > 0 && 
-            votings[votingId].endDate < block.timestamp,
-            "the voting can't be finished yet"
+            voting.endDate > 0 && 
+            voting.endDate < block.timestamp,
+            "The voting can't be finished yet"
         );
 
-        votings[votingId].finished = true;
+        voting.state = VotingState.CalculatingResults;
 
         (uint firstWinner, bool twoWinnersSituation) = _findWinner(votingId);
         if (twoWinnersSituation) {
-            //it is finished, but there is no winner
-            //TODO: send money back to the voters
-            return;
+            for (uint i = 0; i < _givenVotes[votingId].length; i++) {
+                _transfer(
+                    _applyComission(_voteFee), 
+                    _givenVotes[votingId][i].owner, 
+                    votingId
+                );
+            }
+        } else {
+            voting.winner = voting.candidates[firstWinner];
+            _transfer(
+                _applyComission(_balance[votingId]), 
+                voting.winner, 
+                votingId
+            );
         }
-
-        votings[votingId].winner = votings[votingId].candidates[firstWinner];
-        //TODO: send 90% to the winner
+        voting.state = VotingState.Finished;
     }
 
-    function _getIndexOf(address a, address[] memory all) 
-        internal 
-        pure 
-        returns (uint)
-    {
-        for (uint i = 0; i < all.length; i++) {
-            if (all[i] == a) { return i; }
-        }
-
-        revert("no such address in the collection");
+    //@dev transfers the amount from the voting balance to the specified address
+    function _transfer(uint amount, address to, uint votingId) internal {
+        _balance[votingId] -= amount;
+        payable(to).transfer(amount);        
     }
 
     //@dev calculates the votes using internal structures and
@@ -117,12 +147,12 @@ contract VotingFactory is Ownable {
     //@return index of the first winner in the Voting's list of candidates
     function _findWinner(uint votingId) internal view returns (uint, bool) {
         uint[] memory votesFor;
-        for (uint i = 0; i < votings[votingId].candidates.length; i++) {
+        for (uint i = 0; i < _votings[votingId].candidates.length; i++) {
             votesFor[i] = 0;
         }
         for (uint i = 0; i < _givenVotes[votingId].length; i++) {
             address c = _givenVotes[votingId][i].candidate;
-            uint index = _getIndexOf(c, votings[votingId].candidates);
+            uint index = _getIndexOf(c, _votings[votingId].candidates);
             votesFor[index]++;
         }
 
@@ -143,5 +173,23 @@ contract VotingFactory is Ownable {
         }
 
         return (firstMaxIndex, twoWinnersSituation);
+    }
+
+    function _getIndexOf(address a, address[] memory all) 
+        internal 
+        pure 
+        returns (uint)
+    {
+        for (uint i = 0; i < all.length; i++) {
+            if (all[i] == a) { 
+                return i; 
+            }
+        }
+
+        revert("no such address in the collection");
+    }
+
+    function _applyComission(uint input) internal view returns (uint) {
+        return input * (100 - _comission) / 100;
     }
 }
--- test/VotingFactory.test.js
+++ test/VotingFactory.test.js
@@ -5,74 +5,91 @@ describe("VotingFactory", function () {
   let owner
   let cand1, cand2
   let p1, p2, p3
-  let votingFactory
+  let votingSys
 
   beforeEach(async function() {
     [owner, cand1, cand2, p1, p2, p3] = await ethers.getSigners();
     const VotingFactory = 
       await ethers.getContractFactory("VotingFactory", owner);
-    votingFactory = await VotingFactory.deploy();
-    await votingFactory.deployed();
+    votingSys = await VotingFactory.deploy();
+    await votingSys.deployed();
   })
   
-  it("should be deployed", async function () {
-    expect(votingFactory.address).is.properAddress;
+  it("should set duration only by owner", async function () {
+    await checkCallByOwnerOnly(async function (sender) { 
+      await votingSys.connect(sender).setVotingDuration(5);
+    });
+  });
+  
+  it("should add a new voting only by owner", async function () {
+    await checkCallByOwnerOnly(async function (sender) {
+      await votingSys.connect(sender).addVoting([cand1.address, cand2.address]);
+      let v = await votingSys.getVotings();
+      expect(v.length).eq(1);
+    });
   });
 
-  it("should NOT add a new voting without candidates", async function () {
+  it("should require candidates for a new voting", async function () {
     let err;
     try {
-      await votingFactory.addVoting([]);
+      await votingSys.addVoting([]);
     } catch (error) {
       err = error;
     }
     expect(err).is.not.undefined;
-  });
-
-  it("should be possible to add a new voting only by owner", async function () {
-    let err;
-    try {
-      await votingFactory.connect(p1).addVoting([cand1.address]);
-    } catch (error) {
-      err = error;
-    }
-    expect(err).is.not.undefined;
-  });
-
-  it("should add a new voting", async function () {
-    await votingFactory.addVoting([cand1.address, cand2.address]);
-    let v = await votingFactory.getVotings();
-    expect(v.length).eq(1);
   });
 
   it("should be possible to have more than 1 voting", async function () {
-    await votingFactory.addVoting([cand1.address, cand2.address]);
-    await votingFactory.addVoting([cand1.address, cand2.address]);
+    await votingSys.addVoting([cand1.address, cand2.address]);
+    await votingSys.addVoting([cand1.address, cand2.address]);
 
-    let v = await votingFactory.getVotings();
+    let v = await votingSys.getVotings();
     expect(v.length).eq(2);
   });
  
   it("should add a new vote from p1 for cand1", async function () {
-    await votingFactory.addVoting([cand1.address, cand2.address]);
+    await votingSys.addVoting([cand1.address, cand2.address]);
     let votingId = 0;
-    await votingFactory.connect(p1).vote(votingId, cand1.address);
+    await votingSys.connect(p1).vote(votingId, cand1.address);
 
-    let v = await votingFactory.getVotingDetails(0);
-    expect(v[1].length).eq(1);
+    let v = await votingSys.getVotingDetails(votingId);
+    let votes = v[1];
+    expect(votes.length).eq(1);
+    expect(votes[0].owner).eq(p1.address);
+    expect(votes[0].candidate).eq(cand1.address);
   });
 
-  it("should NOT add a second vote from p1", async function () {
-    await votingFactory.addVoting([cand1.address, cand2.address]);
+  it("shouldn't exceed vote limit from p1", async function () {
+    //by default the limit = 1
+    await votingSys.addVoting([cand1.address, cand2.address]);
     let votingId = 0;
-    await votingFactory.connect(p1).vote(votingId, cand1.address);
+    await votingSys.connect(p1).vote(votingId, cand1.address);
 
     let err;
     try {
-      await votingFactory.connect(p1).vote(votingId, cand2.address);
+      await votingSys.connect(p1).vote(votingId, cand2.address);
     } catch (error) {
       err = error;
     }
     expect(err).is.not.undefined;
   });
+
+  async function checkCallByOwnerOnly(a) {
+    let unexpectedError;
+    try {
+      
+      await a(owner);
+    } catch (error) {
+      unexpectedError = error;
+    }
+    expect(unexpectedError).is.undefined;
+
+    let expectedError;
+    try {
+      await a(p1);
+    } catch (error) {
+      expectedError = error;
+    }
+    expect(expectedError).is.not.undefined;
+  }
 });
